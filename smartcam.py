f45e8d3d0dcdbefe9f1e9b23433e61843f202454
Theerawat Ramchuen <ramchuen@gmail.com>
2022-05-23 15:53:12 +0700

    original is detectnet.py from jetson-inference package

---
+++ smartcam.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+# import cv2
+import jetson.inference
+import jetson.utils
+
+import argparse
+import sys
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# create video output object 
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+	
+# load the object detection network
+net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
+
+# create video sources
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+
+import requests
+
+def linenotify(message):
+	url = 'https://notify-api.line.me/api/notify'
+	token = 'cFUmUjOpYUAKi2XaPfRHeseZoyPQUSV6FNOOrUJ03cQ' # Line Notify smartcam token "cFUmUjOpYUAKi2XaPfRHeseZoyPQUSV6FNOOrUJ03cQ"
+	img = {'imageFile': open('image2send.jpg','rb')} #Local picture File
+	data = {'message': message}
+	headers = {'Authorization':'Bearer ' + token}
+	session = requests.Session()
+	session_post = session.post(url, headers=headers, files=img, data =data)
+	print(session_post.text) 
+
+# process frames until the user exits
+while True:
+	# capture the next image
+	img = input.Capture()
+
+	# detect objects in the image (with overlay)
+	detections = net.Detect(img, overlay=opt.overlay)
+
+	# print the detections
+	print("detected {:d} objects in image".format(len(detections)))
+
+	for detection in detections:
+		print(detection)
+		if detection.ClassID == 1: ## If found Person
+			print("saving found person image...")
+			jetson.utils.saveImageRGBA('image2send.jpg',img) #result=cv2.imwrite(r'image2send.jpg',img )
+			linenotify("Hello. How are you")
+			break
+
+	# render the image
+	output.Render(img)
+
+	# update the title bar
+	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+
+	# print out performance info
+	net.PrintProfilerTimes()
+
+	# exit on input/output EOS
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
+
+
